ff01563967bf0c9c6dc70966bc8bd8ae099616e1
Martin R <sizlaksystems@gmail.com>
2016-09-07 10:34:44 +0200

    Adding .user.ini to prevent timeouts

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -11,9 +11,12 @@ $databases = array (
       'password' => $db["pass"],
       'host' => $db["host"],
       'port' => '3306',
-    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+      'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
       'driver' => 'mysql',
       'prefix' => '',
     ),
   ),
 );
+
+# setting bigger timeout for install
+ini_set('max_execution_time', 3600);
